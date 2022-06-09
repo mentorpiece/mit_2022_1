92af952efffeb98538864ef3955c13278e8c3dc1
Nikolay Gladnev <ngladnev@example.com>
2022-06-09 15:03:32 +0400

    fix script

--- get_sudent_info.sh
+++ get_sudent_info.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-input="author.csv"
+input="students.csv"
 
 echo "STUDENTS:"
 while IFS= read -r line
 do
-  #echo "-- User:   $line"| tr "a-z" "A-Z"
+  echo "-- User:   $line"| tr "a-z" "A-Z"
   USER_INFO=$(curl -s "https://api.github.com/users/${line}")
-  echo "  | User: $line"
+  # echo "  | User: $line"
   echo "$USER_INFO" | grep API
   echo "$USER_INFO" | grep \"name\" |awk -F '"' '{print " | Name:   " $4}'
   echo "$USER_INFO" | grep \"url\" |awk -F '"' '{print " | GitHub: " $4}'
